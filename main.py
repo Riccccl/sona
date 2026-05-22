5fe4e0af02c4809393a088805d628ec649bb9b2b
riccardozedda <rz@lothar-medtec.de>
2026-05-22 14:16:15 +0200

    Refactor environment variable handling in main function

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ from dotenv import load_dotenv
 from os import getenv
 load_dotenv()
 
-username: str = getenv("SONA_USERNAME")
-password: str = getenv("SONA_PASSWORD")
-webhook_url: str = getenv("DISCORD_WEBHOOK_URL")
-website_link: str = "https://psywue.sona-systems.com/"
-
 def main() -> None:
+    if not all([getenv("SONA_USERNAME"), getenv("SONA_PASSWORD"), getenv("DISCORD_WEBHOOK_URL")]):
+        raise ValueError("Missing required environment variables. Please check your .env file or environment secrets.")
+    username: str = getenv("SONA_USERNAME")
+    password: str = getenv("SONA_PASSWORD")
+    webhook_url: str = getenv("DISCORD_WEBHOOK_URL")
+    website_link: str = "https://psywue.sona-systems.com/"
+
+
     #studies = Scraper.scrape_studies(username, password, website_link)
     send_notification("Hallo aus dem Script", webhook_url)
-    pass
 
 if __name__ == "__main__":
     main()
